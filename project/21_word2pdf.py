bde226dd05b8ca49f4429ea787c1cb351bd8a256
xueda3122356 <112776684+xueda3122356@users.noreply.github.com>
2024-09-20 20:07:25 -0700

    new project-word_to_pdf

---
+++ project/21_word2pdf.py
@@ -0,0 +1,22 @@
+# pip install pywin32
+from win32com.client import gencache
+from win32com.client import constants, gencache
+
+def createPdf(wordPath, pdfPath):
+    """
+    word 转 pdf
+    :pargm wordPath: word文件路径
+    :pargm pdfPath: pdf保存路径
+    """
+
+    word = gencache.EnsureDispatch('Word.Application')
+    doc = word.Documents.Open(wordPath, ReadOnly = 1)
+    doc.ExportAsFixedFormat(pdfPath,
+                            constants.wdExportFormatPDF,
+                            Item = constants.wdExportDocumentWithMarkup,
+                            CreateBookmarks = constants.wdExportCreateHeadingBookmarks)
+    word.Quit(constants.wdDoNotSaveChanges)
+
+if __name__ == "__main__":
+    createPdf('F:/python/base_data/traffic_violation_notice.docx', 'F:/python/generate_data/21_word2pdf.pdf')
+
